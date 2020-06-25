6de5b31914cebadf3da86b7739ed36431baa1a0f
Your Name <you@example.com>
2020-06-24 20:22:54 -0700

    firstcommit

---
+++ js/header.js
@@ -0,0 +1,17 @@
+window.onload = function onload() {
+    let isToggle = false;
+
+    document.querySelector('.toggle') .addEventListener('click', function onClick() {
+        let val = '';
+
+        if (!isToggle) {
+            val = 'block';
+        
+        } else {
+            val = 'none';
+        }
+        isToggle = !isToggle;
+       document.querySelector('.nav').style.display = val;
+       
+    });
+}
